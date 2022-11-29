6d3f76cb75c2cb8b3672bf94a8f6c051e269f825
tayefulislam <kazitayefulislam12@gmail.com>
2022-11-30 01:15:52 +0600

    initial redux and redux setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  const test = useSelector((state) => state);
+
+  console.log(test);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <p>This is Home</p>
+      {data?.map((product, index) => (
+        <ProductCard product={product} key={index}></ProductCard>
+      ))}
     </div>
   );
 };
---
+++ src/redux/reducers/ProductReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  test: "test",
+};
+
+const ProductReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default ProductReducer;
