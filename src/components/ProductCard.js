bb5b5c6c707899aa73d76a78154e99f5929d4a87
tayefulislam <kazitayefulislam12@gmail.com>
2022-12-04 13:10:53 +0600

    redux fully setup

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 import {
   addToCart,
   removeFromCart,
@@ -8,11 +9,17 @@ import {
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
+  // console.log(pathname);
   return (
     <div
       className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
       key={product._id}
     >
+      <div className="">
+        <p>{product.quantity}</p>
+      </div>
       <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
@@ -30,19 +37,23 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-          onClick={() => dispatch(removeFromCart(product))}
-        >
-          Remove
-        </button>
+        {!pathname.includes("cart") && (
+          <button
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+            onClick={() => dispatch(removeFromCart(product))}
+          >
+            Remove
+          </button>
+        )}
 
-        <button
-          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-          onClick={() => dispatch(addToCart(product))}
-        >
-          Add to cart
-        </button>
+        {!pathname.includes("cart") && (
+          <button
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+            onClick={() => dispatch(addToCart(product))}
+          >
+            Add to cart
+          </button>
+        )}
         <button
           title="Add to wishlist"
           className="bg-indigo-500  py-1 px-2 rounded-full"
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -15,25 +15,47 @@ export const productReducer = (state = initialState, action) => {
   //   console.log(selectProduct);
   switch (action.type) {
     case ADD_TO_CART:
+      if (selectProduct) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== action.payload._id
+        );
+
+        selectProduct.quantity += 1;
+
+        return {
+          ...state,
+          cart: [...newCart, selectProduct],
+        };
+      }
+
+      action.payload.quantity = 1;
       return {
         ...state,
         cart: [...state.cart, action.payload],
       };
 
     case REMOVE_FROM_CART:
-      //   if (selectProduct) {
-      //     const newCart = state.cart.filter(
-      //       (product) => product._id !== selectProduct._id
-      //     );
+      if (selectProduct.quantity > 1) {
+        console.log(selectProduct);
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectProduct._id
+        );
 
-      //     return {
-      //       ...newCart,
-      //       cart: [newCart],
-      //     };
-      //   }
+        selectProduct.quantity -= 1;
+
+        return {
+          ...state,
+          cart: [...newCart, selectProduct],
+        };
+      }
+
+      const newCart = state.cart.filter(
+        (product) => product._id !== action.payload._id
+      );
+
       return {
         ...state,
-        cart: [...state.cart],
+        cart: [...newCart],
       };
 
     default:
