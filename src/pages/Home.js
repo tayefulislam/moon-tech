b7cf690e6f8a37a137bb6cf620eeb8bad67f667f
tayefulislam <kazitayefulislam12@gmail.com>
2022-12-06 00:37:33 +0600

    link udated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("products.json")
+    fetch("http://localhost:5000/products")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
