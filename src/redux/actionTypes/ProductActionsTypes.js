d1554264475fda7176ff1b408ed3b37ed15867f4
tayefulislam <kazitayefulislam12@gmail.com>
2022-12-07 23:54:22 +0600

    initial state updated

--- src/redux/actionTypes/ProductActionsTypes.js
+++ src/redux/actionTypes/ProductActionsTypes.js
@@ -1,2 +1,7 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+// brands and toggles
+
+export const TOGGLE_BRAND = "TOGGLE_BRAND";
+export const TOGGLE_STOCK = "TOGGLE_STOCK";
--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -1,7 +1,23 @@
-const initialState = { test: "TEST" };
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/ProductActionsTypes";
+
+const initialState = {
+  filters: {
+    brands: "",
+    stock: "",
+  },
+  keyword: "",
+};
 
 const filterReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_BRAND:
+      return {};
+
+    case TOGGLE_STOCK:
+      return {};
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
