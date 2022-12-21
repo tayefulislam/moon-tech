d27bda46a151e941f0686e2b7b432404a9f8bb8c
tayefulislam <kazitayefulislam12@gmail.com>
2022-12-22 01:50:22 +0600

    thunk pack added

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,11 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 import logger from "redux-logger";
 import cartCounter from "./middleware/cartCounter";
+import thunk from "redux-thunk";
 //  composeWithDevTools()
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(cartCounter))
+  composeWithDevTools(applyMiddleware(cartCounter, thunk, logger))
 );
 
 export default store;
